b98b84468a2305115aae8e4f55752f7850a1baa9
Kush <bvcoemit@gamil.com>
2024-01-18 23:20:15 +0530

    formatted files
    `

--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,11 +1,11 @@
 // import { Link } from "react-router-dom";
 
-import { useStarredShows } from "../lib/useStarredShows";
+import { useStarredShows } from '../lib/useStarredShows';
 
 const Starred = () => {
-   const [ starredShows ] = useStarredShows();
+   const [starredShows] = useStarredShows();
 
-   return <div>Starred shows, starred {starredShows.length}</div>
+   return <div>Starred shows, starred {starredShows.length}</div>;
 };
 
 export default Starred;
